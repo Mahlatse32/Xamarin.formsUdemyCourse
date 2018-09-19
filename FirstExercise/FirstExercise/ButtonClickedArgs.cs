e7ad755228e8116030ccbae2651e61fbe2b814f6
Mahlatse <31064845+Mahlatse32@users.noreply.github.com>
2018-09-19 11:34:42 +0200

    I have binded the font size to the slide

---
+++ FirstExercise/FirstExercise/ButtonClickedArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstExercise
+{
+    public class ButtonClickedArgs : EventArgs
+    {
+        public string quotes { get; set; }
+    }
+}
--- FirstExercise/FirstExercise/QuotesPage.xaml.cs
+++ FirstExercise/FirstExercise/QuotesPage.xaml.cs
@@ -17,23 +17,31 @@ namespace FirstExercise
 			InitializeComponent ();
 		}
 
-        public static void Quotes(int x)
+        
+
+        public static void QuotesData()
         {
-            string[] quotes = 
-            {
-                "The past can’t hurt you anymore, not unless you let it",
-                "FOrtune favours the brave",
-                "Did you ever stop to think, and forget to start again?",
-                "I’m not bad. I’m just drawn that way.",
-                "I gave her my heart and she gave me a pen",
-                "Lies are directed to whosoever is disliked",
-                "Boredom happens only when you have so much time available",
-                "love is like taking one step backwards and taking two steps up.",
-                "Be happy for this moment. This moment is your life",
-                "We loved with a love that was more than love",
-                "To find the person you can truly trust, look no further that the nearest mirror",
-                "Better to understand little than to misunderstand a lot"
-            };
+
+            List<ButtonClickedArgs> quoteData = new List<ButtonClickedArgs>();
+
+            quoteData.Add(new ButtonClickedArgs { quotes = "The past can’t hurt you anymore, not unless you let it" });
+            quoteData.Add(new ButtonClickedArgs { quotes = "FOrtune favours the brave" });
+            quoteData.Add(new ButtonClickedArgs { quotes = "Did you ever stop to think, and forget to start again?" });
+            quoteData.Add(new ButtonClickedArgs { quotes = "I’m not bad. I’m just drawn that way." });
+            quoteData.Add(new ButtonClickedArgs { quotes = "I gave her my heart and she gave me a pen" });
+            quoteData.Add(new ButtonClickedArgs { quotes = "Lies are directed to whosoever is disliked" });
+            quoteData.Add(new ButtonClickedArgs { quotes = "Boredom happens only when you have so much time available" });
+            quoteData.Add(new ButtonClickedArgs { quotes = "love is like taking one step backwards and taking two steps up." });
+            quoteData.Add(new ButtonClickedArgs { quotes = "Be happy for this moment. This moment is your life" });
+            quoteData.Add(new ButtonClickedArgs { quotes = "We loved with a love that was more than love" });
+            quoteData.Add(new ButtonClickedArgs { quotes = "To find the person you can truly trust, look no further that the nearest mirror" });
+            quoteData.Add(new ButtonClickedArgs { quotes = "Better to understand little than to misunderstand a lot" });
+            quoteData.Add(new ButtonClickedArgs { quotes = "I write what I like" });
         }
-	}
+
+        private void Button_Clicked(object sender, ButtonClickedArgs e)
+        {
+            label.Text = ; 
+        }
+    }
 }
