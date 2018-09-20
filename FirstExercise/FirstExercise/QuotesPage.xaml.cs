74b6373593c8265bc8912bd9e44d9ee537f6a308
Mahlatse <31064845+Mahlatse32@users.noreply.github.com>
2018-09-20 12:10:53 +0200

    Code refactored

--- FirstExercise/FirstExercise/QuotesPage.xaml.cs
+++ FirstExercise/FirstExercise/QuotesPage.xaml.cs
@@ -16,39 +16,38 @@ namespace FirstExercise
 		public QuotesPage ()
 		{
 			InitializeComponent ();
-            label.Text = QuotesData(0);
+            label.Text = quotes[count];
 		}
 
         int count = 0;
-        public static string QuotesData(int x)
+
+        private string[] quotes = new string[]
         {
-            List<ButtonClickedArgs> quoteData = new List<ButtonClickedArgs>();
-
-            quoteData.Add(new ButtonClickedArgs { quotes = "The past can’t hurt you anymore, not unless you let it" });
-            quoteData.Add(new ButtonClickedArgs { quotes = "Fortune favours the brave" });
-            quoteData.Add(new ButtonClickedArgs { quotes = "Did you ever stop to think, and forget to start again?" });
-            quoteData.Add(new ButtonClickedArgs { quotes = "I’m not bad. I’m just drawn that way." });
-            quoteData.Add(new ButtonClickedArgs { quotes = "I gave her my heart and she gave me a pen" });
-            quoteData.Add(new ButtonClickedArgs { quotes = "Lies are directed to whosoever is disliked" });
-            quoteData.Add(new ButtonClickedArgs { quotes = "Boredom happens only when you have so much time available" });
-            quoteData.Add(new ButtonClickedArgs { quotes = "love is like taking one step backwards and taking two steps up." });
-            quoteData.Add(new ButtonClickedArgs { quotes = "Be happy for this moment. This moment is your life" });
-            quoteData.Add(new ButtonClickedArgs { quotes = "We loved with a love that was more than love" });
-            quoteData.Add(new ButtonClickedArgs { quotes = "To find the person you can truly trust, look no further that the nearest mirror" });
-            quoteData.Add(new ButtonClickedArgs { quotes = "Better to understand little than to misunderstand a lot" });
-            quoteData.Add(new ButtonClickedArgs { quotes = "I write what I like" });
-
-            return quoteData[x].quotes;
-        }
+            "The past can’t hurt you anymore, not unless you let it",
+            "Fortune favours the brave",
+            "Did you ever stop to think, and forget to start again?",
+            "I’m not bad. I’m just drawn that way.",
+            "I gave her my heart and she gave me a pen",
+            "Lies are directed to whosoever is disliked",
+            "Boredom happens only when you have so much time available",
+            "love is like taking one step backwards and taking two steps up.",
+            "Be happy for this moment. This moment is your life",
+            "We loved with a love that was more than love",
+            "To find the person you can truly trust, look no further that the nearest mirror",
+            "Better to understand little than to misunderstand a lot",
+            "I write what I like"
+        };
 
         private void Button_Clicked(object sender, ButtonClickedArgs e)
         {
-            if(count == 12)
-            {
-                count = -1;
-            }
             count++;
-            label.Text = QuotesData(count);
+
+            if (count >= quotes.Length)
+            {
+                count = 0;
+            }
+
+            label.Text = quotes[count];
         }
     }
 }
